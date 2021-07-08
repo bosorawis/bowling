3f8fb3a7b12de2e1bfec1992eef1577e729414cd
dihmuzikien <turbo2547@hotmail.com>
2021-07-07 18:17:32 -0700

    big refactor

--- score.go
+++ score.go
@@ -2,55 +2,135 @@ package bowling
 
 import (
 	"fmt"
-	"strconv"
 )
 
 var validInput = []rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', 'x', 'X'}
+
 var (
 	ErrValidation = fmt.Errorf("invalid input")
 )
 
-type game struct {
-	rolls      []string
-	fallenPins []int
+const (
+	totalPins = 10
+	maxFrame  = 10
+	maxThrows = 21
+)
+
+type frame struct {
+	downPins       []int
+	score          int
+	remainingBonus int
+	number         int
 }
 
-func (g *game) Record(roll string) error {
+func (f *frame) completed() bool {
+	return f.remainingBonus <= 0
+}
 
+func strikeFrame(rolls []int, num int) *frame {
+	frame := &frame{
+		downPins:       rolls,
+		score:          sum(rolls),
+		number:         num,
+	}
+
+	if len(rolls) == 1 && rolls[0] == 10 { // strike
+		frame.remainingBonus = 2
+	} else if len(rolls) == 2 && rolls[0] + rolls[1] == 10 {
+		frame.remainingBonus = 1
+	} else {
+		frame.remainingBonus = 0
+	}
+	return frame
+}
+func newFrame(rolls []int, num int) *frame {
+	return &frame{
+		downPins:       []int{totalPins},
+		score:          10,
+		remainingBonus: 1,
+		number:         num,
+	}
+}
+
+type game struct {
+	currentFrame int
+	frames       [maxFrame]*frame
+	scoringQueue []*frame
+}
+
+func newGame() *game {
+	return &game{
+		currentFrame: 0,
+		frames:       [maxFrame]*frame{},
+	}
+}
+
+func (g *game) calculateBonus(rolls []int){
+	for i := 0 ; i < len(g.scoringQueue); i++ {
+		scoreFrame := g.scoringQueue[i]
+		for j := 0; j < len(rolls); j++ {
+			if scoreFrame.remainingBonus <= 0 {
+				break
+			}
+			scoreFrame.score += rolls[j]
+			scoreFrame.remainingBonus -= 1
+		}
+		if scoreFrame.remainingBonus <= 0 {
+			g.frames[scoreFrame.number] = scoreFrame
+		}
+	}
+	temp := g.scoringQueue[:0]
+	for _, v := range g.scoringQueue {
+		if v.remainingBonus > 0 {
+			temp = append(temp, v)
+		}
+	}
+	g.scoringQueue = temp
+}
+
+func (g *game) Record(rolls []int) error {
+	if len(rolls) > 3 {
+		return fmt.Errorf("cannot throw more than 3 balls")
+	}
+
+	var newFrame *frame
+	g.calculateBonus(rolls)
+	if len(rolls) == 1 && rolls[0] == 10 { // strike
+		newFrame = strikeFrame(g.currentFrame)
+		g.scoringQueue = append(g.scoringQueue, newFrame)
+	} else if len(rolls) == 2 && rolls[0] + rolls[1] == 10{ // spare
+
+	} else {
+		newFrame = &frame{
+			downPins: rolls,
+			score:    sum(rolls),
+		}
+	}
+
+	g.frames[g.currentFrame] = newFrame
+	g.currentFrame += 1
 	return nil
 }
 
-//func validateRoll(s string) error {
-//	return nil
-//}
+func sum(arr []int) (total int) {
+	for _, v := range arr {
+		total += v
+	}
+	return
+}
 
-func rollToFallenPins(roll string) ([]int, error) {
-	scores := []int{}
-	for i, c := range roll {
-		if c == 'x' || c == 'X' {
-			scores = append(scores, 10)
-		} else if c == '/' {
-			scores = append(scores, 10-scores[i-1])
-		} else {
-			sc, err := strconv.Atoi(string(c))
-			if err != nil {
-				return nil, err
-			}
-			scores = append(scores, sc)
+func (g *game) Score() []int {
+	frameScores := []int{}
+	for i := 0; i < g.currentFrame; i++ {
+		if g.frames[i].completed(){
+			frameScores = append(frameScores, g.frames[i].score)
 		}
 	}
-	return scores, nil
+	return frameScores
 }
 
-func isStrike(s string) bool {
-	if s[0] == 'X' || s[0] == 'x' {
-		return true
-	}
-	return false
-}
-
-func isSpare(s string) bool {
-	if s[1] == '/' {
+func (g *game) Done() bool {
+	if g.frames[maxFrame-1] != nil {
 		return true
 	}
 	return false
--- score_test.go
+++ score_test.go
@@ -5,128 +5,39 @@ import (
 	"testing"
 )
 
-func TestRollToFallenPin(t *testing.T) {
-	t.Run("successful conversion", func(t *testing.T) {
+func TestRecordAndScore(t *testing.T) {
+	t.Run("Test recording and scoring my bowling game", func(t *testing.T) {
 		testcases := []struct {
-			description string
-			input       string
-			want        []int
+			input [][]int
+			want  []int
 		}{
 			{
-				description: "no strike, no spare",
-				input:       "12",
-				want:        []int{1, 2},
+				input: [][]int{{1, 1}, {2, 2}, {3, 3}},
+				want:  []int{2, 4, 6},
 			},
 			{
-				description: "strike",
-				input:       "x",
-				want:        []int{10},
+				input: [][]int{{10}, {10}, {10}, {5, 3}},
+				want:  []int{30, 25, 18, 8},
 			},
 			{
-				description: "spare",
-				input:       "5/",
-				want:        []int{5, 5},
+				input: [][]int{{10}, {10}},
+				want:  []int{},
 			},
 			{
-				description: "three throws",
-				input:       "1/9",
-				want:        []int{1, 9, 9},
+				input: [][]int{{5, 5}, {5, 5}, {2, 3}},
+				want:  []int{15, 12, 5},
 			},
-			{
-				description: "missed",
-				input:       "50",
-				want:        []int{5, 0},
-			},
-		}
-		for _, tc := range testcases {
-			got, err := rollToFallenPins(tc.input)
-			if err != nil {
-				t.Errorf("unexpected error %v", err)
-			}
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("want %v got %v", tc.want, got)
-			}
-		}
-	})
 
-}
-
-func TestCalculateScore(t *testing.T) {
-
-	//t.Run("Test Validate Roll", func(t *testing.T) {
-	//	testcases := []struct {
-	//		description   string
-	//		input         string
-	//		expectedError error
-	//	}{
-	//		{
-	//			description:   "valid roll strike",
-	//			input:         "x",
-	//			expectedError: nil,
-	//		},
-	//		{
-	//			description:   "valid roll spare",
-	//			input:         "3/",
-	//			expectedError: nil,
-	//		},
-	//		{
-	//			description:   "valid roll points",
-	//			input:         "35",
-	//			expectedError: nil,
-	//		},
-	//		{
-	//			description:   "valid roll - last play",
-	//			input:         "3/5",
-	//			expectedError: nil,
-	//		},
-	//		{
-	//			description:   "invalid roll - invalid character",
-	//			input:         "ab",
-	//			expectedError: ErrValidation,
-	//		},
-	//		{
-	//			description:   "invalid roll - strike on second throw",
-	//			input:         "3x",
-	//			expectedError: ErrValidation,
-	//		},
-	//		{
-	//			description:   "invalid roll - 3 throws specified when shouldn't",
-	//			input:         "345",
-	//			expectedError: ErrValidation,
-	//		},
-	//	}
-	//	for _, tc := range testcases {
-	//		err := validateRoll(tc.input)
-	//		if err != tc.expectedError{
-	//			t.Errorf("input %s: expect %v got %v", tc.input, tc.expectedError, err)
-	//		}
-	//	}
-	//})
-	t.Run("Test Record and Get scores frame", func(t *testing.T) {
-		testcases := []struct {
-			description string
-			throws      []string
-			expected    [10]int
-		}{
-			{
-				description: "no roll returns all 0's",
-				expected:    [10]int{},
-			},
-			{
-				description: "no strikes no spares",
-				throws:      []string{"11", "22"},
-				expected:    [10]int{2, 4},
-			},
 		}
 		for _, tc := range testcases {
 			g := game{}
-			for _, play := range tc.throws {
-				err := g.Record(play)
-				if err != nil {
-					t.Fatalf("unexpected error %v", err)
-				}
+			for _, v := range tc.input {
+				g.Record(v)
+			}
+			got := g.Score()
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("input %v. want %v got %v", tc.input, tc.want, got)
 			}
 		}
 	})
-
 }
